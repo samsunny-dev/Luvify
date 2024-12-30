f84cb2bc181eafd250d4ec8fb67c2bb00fd6d7e8
Sam Sunny <samsunnywithluv@gmail.com>
2024-12-30 23:46:20 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
-import Footer from './components/footer';
-import { Outlet, useLocation } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
+import Router from './routes';
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       <Navbar />
       <Box flex="1">
         <main>
-          <Outlet/>
+          <Router />
         </main>
       </Box>
       {/* <Footer/> */}
