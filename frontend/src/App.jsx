73875b204df685ca4337b8256027a8fd90e9853f
Libin Seban <libinseban97@gmail.com>
2024-12-30 20:23:50 +0530

    frontend started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
-import Chat from './pages/Chat';
-import Communities from './pages/Communities';
-import Events from './pages/Events';
-import ModerationDashboard from './pages/ModerationDashboard';
+import Footer from './components/footer';
+import { Outlet, useLocation } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <ChakraProvider>
-      <Router>
         <Box minHeight="100vh" display="flex" flexDirection="column">
           <Navbar />
           <Box flex="1">
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/chat" element={<Chat />} />
-              <Route path="/communities" element={<Communities />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/moderation-dashboard" element={<ModerationDashboard />} />
-            </Routes>
+          <main>
+            <Outlet/>
+          </main>
           </Box>
-          <Footer />
+          {/* <Footer/> */}
         </Box>
-      </Router>
     </ChakraProvider>
   );
 };
