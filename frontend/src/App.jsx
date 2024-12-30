c60499578b512047b303b608ed9f5204c77b8573
Sam Sunny <samsunnywithluv@gmail.com>
2024-12-30 23:09:10 +0530

    error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import { ChakraProvider, Box, extendTheme } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
 import { Outlet, useLocation } from 'react-router-dom';
 
-const theme = extendTheme({
-  colors: {
-    primary: '#2D3748',
-  },
-});
-
 const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-        <Box minHeight="100vh" display="flex" flexDirection="column">
-          <Navbar />
-          <Box flex="1">
-          <main>
-            <Outlet/>
-          </main>
-          </Box>
-          {/* <Footer/> */}
-        </Box>
-    </ChakraProvider>
+    <Box minHeight="100vh" display="flex" flexDirection="column">
+      <Navbar />
+      <Box flex="1">
+        <main>
+          <Outlet/>
+        </main>
+      </Box>
+      {/* <Footer/> */}
+    </Box>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './styles/global.scss';  // Global styles for the application
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
